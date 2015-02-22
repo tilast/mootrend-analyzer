2de1f4335293b67ad999d5de9bbe066e851f3815
Ihor Kroosh <ihor.kroosh@gmail.com>
2015-02-22 03:19:10 +0200

    nedd to remove psv

--- from_psv_to_mongo.py
+++ from_psv_to_mongo.py
@@ -1,4 +1,5 @@
 import pymongo
+import codecs
 
 connection_string = "mongodb://localhost:27017"
 connection = pymongo.MongoClient(connection_string)
@@ -6,3 +7,30 @@ database = connection.psv_data
 
 psv_items = database.psv_items
 
+csvFile = open('new_tweets.csv', 'r')
+
+item = {}
+flag = False
+for line in csvFile:
+  penisesParts = line.split(':=>')
+  print len(penisesParts)
+  if(flag):
+    if(len(penisesParts) > 0):
+      try:
+        item["tweet"] = item["tweet"].encode('utf8')
+        psv_items.insert(item)
+      except Exception, e:
+        print "except"
+        print e
+      item = {}
+      flag = False
+    else:
+      item["tweet"] += " " + line
+      flag = True
+      continue
+  
+  if(len(penisesParts) == 3):
+    item["city"] = penisesParts[0]
+    item["trend"] = penisesParts[1]
+    item["tweet"] = penisesParts[2]
+    flag = True
